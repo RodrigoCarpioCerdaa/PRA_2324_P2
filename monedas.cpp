325fba31180c566aac16103e86fdf2a3f888c737
Rodrigo Carpio Cerdá <rcarcer@epsa.upv.es>
2023-10-25 23:56:55 -0800

    el monedas.cpp

---
+++ monedas.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <limits>
+#include <algorothm>
+
+int infinito = std::numeric_limits<int>::max();
+
+using namespace std;
+
+int cambio(int v[], int N, int M){
+	int ROWS=N;
+	int** matrix = new int[ROWS];
+	for(int i=0; i<ROWS; i++){
+		matrix[i] = new int[COLUMNS];
+	}
+	if()
+	return M;
+};
+
+int main(){
+	int N=3;
+	int v[N]={1, 4, 6};
+	int M=8;
+	cambio(v, N, M);
+
+
+	return 0;
+};
+
+
+
+
+
